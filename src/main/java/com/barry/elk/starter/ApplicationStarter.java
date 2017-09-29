0cbe39599fc349bce71642ac494dff61b301095c
BarryHan363136 <htbyanghang2@163.com>
2017-09-29 12:04:13 +0800

    init files

---
+++ src/main/java/com/barry/elk/starter/ApplicationStarter.java
@@ -0,0 +1,39 @@
+package com.barry.elk.starter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import javax.servlet.Filter;
+
+/**
+ * TaskApplication [spring boot] 主方法
+ *
+ * 启动方式，右键->run/debug->Spring Boot App
+ *
+ * @author chongwenjun
+ *
+ */
+@SpringBootApplication
+@ComponentScan(basePackages={"com.barry.elk"})
+public class ApplicationStarter {
+    /** 日志类 */
+    private static final Logger logger = LoggerFactory.getLogger(ApplicationStarter.class);
+
+    // 用于处理编码问题
+    @Bean
+    public Filter characterEncodingFilter() {
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+        return characterEncodingFilter;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationStarter.class, args);
+    }
+
+}
